ee657d11696fedab5ba54333e4acca2f35bbe84e
刺身魚 <atto_999@hotmail.com>
2015-11-23 03:48:03 +0900

    Changed log path

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,5 @@
 export DISPLAY=:0
 export `dbus-launch | grep ADDRESS` 
 export `dbus-launch | grep PID` 
-jackd -d alsa > ~/Desktop/jackd.log 2>&1 &
+jackd -d alsa > $(dirname $0)/log/jackd.log 2>&1 &
 
